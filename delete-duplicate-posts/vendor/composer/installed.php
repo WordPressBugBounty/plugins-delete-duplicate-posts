b0692b104970caad105de87474a1d8a22a8b1f4b
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 15:46:50 +0000

    4.9.9

---
+++ delete-duplicate-posts/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '1d8f1dbcb243e5e0ac496f0aa1cb2aa8f916a4e3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '1d8f1dbcb243e5e0ac496f0aa1cb2aa8f916a4e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
